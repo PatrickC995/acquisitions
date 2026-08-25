56aa8e126dbf278b266931093551c2387580cf93
Patrick Campbell <campbellpatrick2004@gmail.com>
2026-08-25 20:27:21 +0100

    adding AUTH

---
+++ src/utils/format.js
@@ -0,0 +1,7 @@
+export const formatvalidationError = (errors) => {
+    if (!errors || !Array.isArray(errors)) {
+        return 'Validation failed';
+    }
+
+    return errors.map(i => i.message).join(', ');
+};
